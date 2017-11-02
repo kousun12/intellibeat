8d72e51e3e650ffa0116673daba8008f62375f21
Rob Cheung <rob@finxpc.com>
2017-11-02 16:31:10 -0700

    --wip-- [skip ci]

---
+++ src/main/java/com/robc/intellibeat/KeystrokeAction.java
@@ -0,0 +1,11 @@
+package com.robc.intellibeat;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class KeystrokeAction extends AnAction {
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+  }
+}
---
+++ src/main/java/com/robc/intellibeat/listeners/Restartable.java
@@ -0,0 +1,7 @@
+package com.robc.intellibeat.listeners;
+
+public interface Restartable {
+	void start();
+
+	void stop();
+}
